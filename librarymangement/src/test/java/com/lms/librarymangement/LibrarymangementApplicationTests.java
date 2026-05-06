3ad38749ffb0b4f7fb44581f50b4473c9633a0b3
Cristian Quejada <cristianquejada@gmail.com>
2026-05-06 16:06:13 +0800

    initial commit

---
+++ librarymangement/src/test/java/com/lms/librarymangement/LibrarymangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.librarymangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarymangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
