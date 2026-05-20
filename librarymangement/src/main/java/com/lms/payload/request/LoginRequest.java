22ef6f2e400ce60cb789e90e8f2daf6952e4a0bb
Cristian Quejada <cristianquejada@gmail.com>
2026-05-20 17:07:38 +0800

    Added new files for password reset functionality and made modifications to existing files to integrate the new features. The changes include updates to the AuthService, CustomUserServiceImpl, SecurityConfig, UserRepository, and application properties. Additionally, there are some untracked files related to user service and data initialization.

---
+++ librarymangement/src/main/java/com/lms/payload/request/LoginRequest.java
@@ -0,0 +1,17 @@
+package com.lms.payload.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotNull(message = "Email is required")
+    private String email;
+    @NotNull(message = "Password is required")
+    private String password;
+}
