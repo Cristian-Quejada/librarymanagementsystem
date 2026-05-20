773dee08870ef4d852e3d05f131a2625c1b3290c
Cristian Quejada <cristianquejada@gmail.com>
2026-05-20 17:58:25 +0800

    Added UserService and implementation

---
+++ librarymangement/src/main/java/com/lms/Service/UserService.java
@@ -0,0 +1,13 @@
+package com.lms.Service;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import com.lms.Model.User;
+import com.lms.payload.dto.UserDto;
+
+public interface UserService {
+
+    public User getCurrentUser() throws Exception;
+    public List<UserDto> getAllUsers();
+}
---
+++ librarymangement/src/main/java/com/lms/Service/impl/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.lms.Service.impl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import com.lms.Model.User;
+import com.lms.Service.UserService;
+import com.lms.mapper.UserMapper;
+import com.lms.payload.dto.UserDto;
+import com.lms.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+    @Override
+    public User getCurrentUser() throws Exception {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new Exception("User not found");
+        }
+        return user;
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        List<User> users = userRepository.findAll();
+
+        return users.stream().map(UserMapper::toUserDto).collect(Collectors.toList());
+    }
+
+}
