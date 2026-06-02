defc603c57cbfc2c085ebbdd76c4a96213432845
Cristian Quejada <cristianquejada@gmail.com>
2026-06-03 07:41:18 +0800

    Added EnableAsync to main app class

--- librarymangement/src/main/java/com/lms/librarymangement/LibrarymangementApplication.java
+++ librarymangement/src/main/java/com/lms/librarymangement/LibrarymangementApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(scanBasePackages = "com.lms")
 @EnableJpaRepositories(basePackages = "com.lms.repository")
 @EntityScan(basePackages = "com.lms.Model")
+@EnableAsync
 public class LibrarymangementApplication {
 
 	public static void main(String[] args) {
